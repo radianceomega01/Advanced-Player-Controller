166f008a39a05e8fdcdeff1faaddd75daa2ef18b
Ritansh Bangre <43597951+radianceomega01@users.noreply.github.com>
2023-02-21 17:25:39 +0530

    Fixed Jumping state

--- Assets/Scripts/Player/PlayerStates/In Air/JumpedState.cs
+++ Assets/Scripts/Player/PlayerStates/In Air/JumpedState.cs
@@ -3,35 +3,21 @@ using UnityEngine;
 
 public class JumpedState : InAirState
 {
-    Vector3 playerForwardDir;
-    Vector3 playerRightDir;
-    float force = 5f;
-
-    protected Vector2 moveInput;
-    protected Vector3 movementDir;
+    float force = 7f;
 
     public JumpedState(Player player) : base(player) { }
 
     public override void OnEnter()
     {
-        moveInput = playerActions.PlayerInput.Move.ReadValue<Vector2>();
-
-        playerRightDir = player.transform.right;
-        playerRightDir.y = 0;
-        playerRightDir.Normalize();
-
-        playerForwardDir = player.transform.forward;
-        playerForwardDir.y = 0;
-        playerForwardDir.Normalize();
-
-        movementDir = (playerRightDir * moveInput.x + playerForwardDir * moveInput.y).normalized;
+        player.SetAnimation("Jump");
     }
 
     public override void PhysicsProcess()
     {
-        player.GetRigidBody().AddForce(force * new Vector3(movementDir.x, 1f, movementDir.z), ForceMode.Acceleration);
-        if(player.GetRigidBody().velocity.y <= 0f)
-            player.SetState(StateFactory.GetFallingState(player));
+        player.GetRigidBody().AddForce(force * Vector3.up, ForceMode.Impulse);
+        player.SetState(StateFactory.GetFallingState(player));
+        /*if (player.GetRigidBody().velocity.y <= 0f)
+            player.SetState(StateFactory.GetFallingState(player));*/
     }
 
     public override void Process()
